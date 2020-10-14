a044b8555f9b541f4a323a2d239980b49f64812b
Tony Powell <anthonypowell@hey.com>
2020-10-14 18:50:12 -0400

    web: Delete un-necessary cra files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 const baseUrl = process.env.REACT_APP_API_HOST || "/";
 
@@ -20,18 +18,9 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div>
+      <header>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
