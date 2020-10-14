7a7397dac8252347073d743fbae2d12af31b427c
Tony Powell <anthonypowell@hey.com>
2020-10-14 19:53:26 -0400

    web: Arrange app layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,24 @@
-import { Box, Grid } from "@chakra-ui/core";
+import { Box, Divider, Grid, Text } from "@chakra-ui/core";
 import React from "react";
+import { Geolocator } from "./components/Geolocator";
+import { IPv4Form } from "./components/IPv4Form";
 
 function App() {
   return (
-    <Grid style={{ placeItems: "center" }} h="100vh" w="100%" bg="gray.50">
+    <Grid style={{ placeItems: "center" }} h="100vh" w="100%">
       <Box
         boxShadow="0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)"
         p={4}
         borderRadius={4}
-      ></Box>
+        bg="gray.50"
+      >
+        <Text fontSize="3xl" px={2} pb={6}>
+          IPv4 Geolocator
+        </Text>
+        <IPv4Form />
+        <Divider />
+        <Geolocator url="" />
+      </Box>
     </Grid>
   );
 }
